fed16506eda3a165acbce4370e4583997aac087d
Trey Terrell <trey.terrell@oregonstate.edu>
2014-06-18 16:51:04 -0700

    Fix spacing.

--- RequestCount.lua
+++ RequestCount.lua
@@ -7,42 +7,42 @@ requestCountForm.RibbonPage = nil;
 connection = nil;
 
 function Init()
-	connection = CreateManagedDatabaseConnection();
-	connection:Connect();
-	interfaceMngr = GetInterfaceManager();
-	requestCountForm.Form = interfaceMngr:CreateForm("RequestCount", "Script");
-	requestCountForm.RibbonPage = requestCountForm.Form:CreateRibbonPage("Request Count");
-	requestCountForm.TextEdit = requestCountForm.Form:CreateTextEdit("Count", "Number of Similar Requests");
-	requestCountForm.TextEdit.ReadOnly = true;
-	requestCountForm.TextEdit.Value = GetCount();
-	requestCountForm.Grid = requestCountForm.Form:CreateGrid("History", "History");
-	requestCountForm.Grid.PrimaryTable = GetHistory();
-	requestCountForm.Form:Show();
-	connection:Dispose();
+  connection = CreateManagedDatabaseConnection();
+  connection:Connect();
+  interfaceMngr = GetInterfaceManager();
+  requestCountForm.Form = interfaceMngr:CreateForm("RequestCount", "Script");
+  requestCountForm.RibbonPage = requestCountForm.Form:CreateRibbonPage("Request Count");
+  requestCountForm.TextEdit = requestCountForm.Form:CreateTextEdit("Count", "Number of Similar Requests");
+  requestCountForm.TextEdit.ReadOnly = true;
+  requestCountForm.TextEdit.Value = GetCount();
+  requestCountForm.Grid = requestCountForm.Form:CreateGrid("History", "History");
+  requestCountForm.Grid.PrimaryTable = GetHistory();
+  requestCountForm.Form:Show();
+  connection:Dispose();
 end
 
 function GetCount()
-	connection.QueryString = GetQuery('COUNT(*)');
-	return connection:ExecuteScalar();
+  connection.QueryString = GetQuery('COUNT(*)');
+  return connection:ExecuteScalar();
 end
 
 function GetQuery(fields)
-	return "SELECT "..fields.." FROM transactions WHERE ((ISSN = '"..ISSN().."' AND ISSN != '') OR (ESPNumber = '"..ESPNumber().."' AND ESPNumber IS NOT NULL AND ESPNumber != '')) AND TransactionNumber != '"..TransactionNumber().."'";
+  return "SELECT "..fields.." FROM transactions WHERE ((ISSN = '"..ISSN().."' AND ISSN != '') OR (ESPNumber = '"..ESPNumber().."' AND ESPNumber IS NOT NULL AND ESPNumber != '')) AND TransactionNumber != '"..TransactionNumber().."'";
 end
 
 function GetHistory()
-	connection.QueryString = GetQuery('*');
-	return connection:Execute();
+  connection.QueryString = GetQuery('*');
+  return connection:Execute();
 end
 
 function ISSN()
-	return GetFieldValue("Transaction", "ISSN");
+  return GetFieldValue("Transaction", "ISSN");
 end
 
 function ESPNumber()
-	return GetFieldValue("Transaction", "ESPNumber");
+  return GetFieldValue("Transaction", "ESPNumber");
 end
 
 function TransactionNumber()
-	return GetFieldValue("Transaction", "TransactionNumber");
+  return GetFieldValue("Transaction", "TransactionNumber");
 end
